a0eac53ddabdce5ea12f209d1ffb3d6e9a3bb405
Thorleif <thorleif.ronningen@gmail.com>
2022-06-11 18:08:07 +0200

    Fist commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,56 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
+import './App.css';
+import {Link, Outlet} from "react-router-dom";
+
+import React, { useState, useEffect, useReducer, useRef } from "react";
+
+function Navigation () {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <nav>
+      <Link to="/">Home</Link>
+      <Link to="/about">About</Link>
+      <Link to="/contact">Contact</Link>
+    </nav>);
 }
 
-export default App;
+export function Home(){
+  return(
+    <div>
+      <Navigation />
+      <h1>My website</h1>
+    </div>
+  )
+}
+
+export function About(){
+  return (
+    <div>
+      <Navigation />
+      <h1>About us</h1>
+      <Outlet />
+      </div>
+  )
+}
+
+export function History(){
+  return (
+    <div> 
+      <h1>Our history </h1>
+      </div>
+  )
+}
+
+export function Contact(){
+  return (
+    <div>
+      <Navigation />
+      <h1>Contact us</h1>
+    </div>
+  )
+}
+
+export function App() {
+  return <Home />
+}
+
+
